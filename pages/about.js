7597c579b4b8d1575e875442709e1a415c58dce5
chandanhm1999 <109410990+chandanhm1999@users.noreply.github.com>
2022-11-07 14:10:56 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>In the world of content marketing, video and image-based content continues to rise in popularity—but nothing has dethroned written content as the foundation of any brand’s digital marketing strategy. There are many different types of written content, from the standard blog to social posts and much more.</p>
+                <p>In the world of content marketing, video and image-based content continues to rise in popularity—but nothing has dethroned written content as the foundation of any brand’s digital marketing strategy. There are many different types of written content, from the standard blog to social posts and much more.</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
